2af50a85997d8a4407b303d5b6b15578ceb4a214
Vyanktesh Dongre <vyankteshdongre10@gmail.com>
2024-07-09 17:03:55 +0530

    minor changes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,63 +1,3 @@
-// import Image from "next/image";
-// import img from 'public/logo.png';
-// import img2 from 'public/X.png';
-// import img3 from 'public/Linkedin.png';
-// import img4 from 'public/fb_2.png';
-// import img5 from 'public/copy.png';
-// import img6 from 'public/Up.png';
-
-// const Footer = () => {
-//     return(
-//         <div className="absolute w-[100%]  h-[287px] top-[1356px]  bg-[#3B3C4A]  ">
-//             <div className="absolute w-[285px] h-[249.67px] bg-[#3B3C4A] left-[69.25px] top-[17.33px]">
-//                 <Image src={img} alt="company logo" style={{ width: '184px', height: '76px' }} />
-//                 <p className="text-[14px] font-small text-[#FFFFFF]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-//                 <div className="py-10 flex ">
-//                     <Image className="" src={img3} alt="company logo" style={{ width: '28px', height: '28px'}} />
-//                     <Image className="ml-3" src={img2} alt="company logo" style={{ width: '28px', height: '28px'}} />
-//                     <Image className="ml-3" src={img4} alt="company logo" style={{ width: '28px', height: '28px'}} />
-//                 </div>
-//                 <div className="flex text-[12px]">
-//                     <Image src={img5} alt="company logo" style={{ width:'16px', height:'16px'}} />
-//                     <p  className="text-[#FFFFFF] ml-2">2023 peppermint. all rights reserved.</p>
-//                 </div>
-
-//                 <div class="absolute w-[92px] h-[164px] top-[11px] left-[410px]">
-//                     <h1 className="mb-4 text-[#FFFFFF] font-bold " >Company</h1>
-//                     <button class=" w-full text-left  py-2 text-[#FFFFFF] ">About</button>
-//                     <button class=" w-full text-left py-2 text-[#FFFFFF] ">Blog</button>
-//                     <button class=" w-full text-left py-2 text-[#FFFFFF] ">Events</button>
-//                     <button class=" w-full text-left py-2 text-[#FFFFFF] ">Careers</button>
-//                 </div>
-
-//                 <div class="absolute w-[108px] h-[135px] top-[11px] left-[590px]">
-//                     <h1 className="mb-4 text-[#FFFFFF] font-bold " >Need Help ?</h1>
-//                     <button class=" w-full text-left  py-2 text-[#FFFFFF] ">Contact Us</button>
-//                     <button class=" w-full text-left py-2 text-[#FFFFFF] ">FAQ</button>
-//                     <button class=" w-full text-left py-2 text-[#FFFFFF] ">All Links</button>
-//                 </div>
-
-//                 <div class="absolute w-[277px] h-[90px] top-[11px] left-[780px]">
-//                     <p className="text-[#FFFFFF] font-bold">Subscribe to Our Newsletter</p>
-//                     <div class="relative mt-3">
-//                         <input type="email" class="w-[277px] h-[48px] px-5 py-2 bg-[#3B3C4A] border border-[#ffffff] rounded-[10px] focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Email Address" />
-//                     </div>
-//                 </div>
-
-//                 <div className="absolute w-[32px] h-[32] top-[11px] left-[1125px]">
-//                     <Image src={img6} alt="company logo" />
-//                 </div>
-
-//                 <div className="absolute w-[173px] h-[18] top-[210px] left-[965px]">
-//                     <p className="text-[12px] text-[#FFFFFF]">Terms Of Use | Privacy Policy </p>
-//                 </div>
-
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Footer;
 
 import Image from "next/image";
 import img from 'public/logo.png';
@@ -131,29 +71,6 @@ const Footer = () => {
 }
 
 export default Footer;
-
-
-
-// import Image from "next/image";
-// import img from 'public/logo.png';
-// import img2 from 'public/X.png';
-// import img3 from 'public/Linkedin.png';
-// import img4 from 'public/fb_2.png';
-// import img5 from 'public/copy.png';
-// import img6 from 'public/Up.png';
-
-// const Footer = () => {
-//     return(
-//         <div className="w-[100%] h-[287px] mx-auto bg-[#3B3C4A] "  >
-//             <div className="">
-
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default Footer;
 
 
 
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,90 +1,5 @@
 "use client"
 
-// import Image from 'next/image';
-// import img from 'public/pepper.svg';
-// import React, { useState } from 'react';
-// import { useRouter } from 'next/router';
-
-
-// const Header = () => {
-//   const [activeMenu, setActiveMenu] = useState(null);
-
-//   // Function to toggle active menu and apply gradien
-//   const toggleGradient = (href) => {
-//     setActiveMenu(href);
-    
-//   };
-
-//   return (
-//     <div className="bg-white flex items-center justify-between px-4" style={{ width: '1280px', height: '86px' }}>
-//       {/* Company Logo */}
-//       <div className="flex items-center">
-//         <Image src={img} alt="company logo" style={{ width: '182px', height: '76px' }} />
-//       </div>
-
-//       {/* Navigation Buttons */}
-//       <div className="relative flex items-center space-x-4 py-1.25"
-//         style={{ width: '912px', height: '66px', top: '10px', padding: '5px' }}>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '48px', height: '24px' }}>
-//           Home
-//         </button>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '74px', height: '24px' }}>
-//           About Us
-//         </button>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '73px', height: '24px' }}>
-//           Solutions
-//         </button>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '72px', height: '24px' }}>
-//           Products
-//         </button>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '78px', height: '24px' }}>
-//           Platforms
-//         </button>
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '80px', height: '24px' }}>
-//           Industries
-//         </button>
-
-//         {/* Resources Button with Dropdown */}
-//         <div className="relative group">
-//           <button className="text-gray-700 hover:text-gray-900 font-medium text-16px leading-24px transition-colors" style={{ width: '84px', height: '24px' }}>
-//             Resources
-//           </button>
-//           <div className="absolute left-0 mt-2 w-[250px] bg-white border border-[#E6E6E6] rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"style={{ top: '42px', padding: '5px', gap: '0px' }}>
-//           <a href="#" className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/product-brochures' ? 'bg-gradient-to-r from-transparent to-blue-500' : ''}`} onClick={() => toggleGradient('/product-brochures')}>
-//           Product Brochures
-//         </a>
-//         <a href="#" className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/case-studies' ? 'bg-gradient-to-r from-transparent to-blue-500' : ''}`} onClick={() => toggleGradient('/case-studies')}>
-//           Case Studies
-//         </a>
-//         <a href="#" className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/videos' ? 'bg-gradient-to-r from-transparent to-blue-500' : ''}`} onClick={() => toggleGradient('/videos')}>
-//           Videos
-//         </a>
-//         <a href="/blogs" style={{ backgroundImage: 'linear-gradient(263.3deg, rgba(255, 255, 255, 0) 7.35%, rgba(116, 205, 219, 0.65) 92.32%)' }} className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/blogs' ? 'bg-gradient-to-bl from-transparent via-transparent to-[rgba(116, 205, 219, 0.65)]' : ''}`} onClick={() => toggleGradient('/blogs')}>
-//           Blogs
-//         </a>
-//         <a href="#" className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/news' ? 'bg-gradient-to-r from-transparent to-blue-500' : ''}`} onClick={() => toggleGradient('/news')}>
-//           News
-//         </a>
-//         <a href="#" className={`block px-4 py-2 text-gray-700 hover:bg-gray-100 ${activeMenu === '/events' ? 'bg-gradient-to-r from-transparent to-blue-500' : ''}`} onClick={() => toggleGradient('/events')}>
-//           Events
-//         </a>
-//           </div>
-//         </div>
-
-//         <button className="text-gray-700 hover:text-gray-900 font-medium text-16px transition-colors" style={{ width: '102px', height: '24px' }}>
-//           Partnerships
-//         </button>
-//         <button className=" rounded-[10px] bg-[#3C3F58] border border-gray-300 text-white" style={{ width: '126px', height: '56px' }}>
-//           Contact Us
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
 import Image from 'next/image';
 import img from 'public/pepper.svg';
 import React, { useState } from 'react';
@@ -152,24 +67,6 @@ const Header = () => {
           </button>
         </nav>
         
-        {/* Mobile Menu Dropdown */}
-        {/* {isMenuOpen && (
-          <div className="md:hidden absolute bg-slate-100 border border-gray-300 py-2 px-4">
-            <MobileNavButton href="/" text="Home" onClick={() => toggleGradient('/')} />
-            <MobileNavButton href="/about" text="About Us" onClick={() => toggleGradient('/about')} />
-            <MobileNavButton href="/solutions" text="Solutions" onClick={() => toggleGradient('/solutions')} />
-            <MobileNavButton href="/products" text="Products" onClick={() => toggleGradient('/products')} />
-            <MobileNavButton href="/platforms" text="Platforms" onClick={() => toggleGradient('/platforms')} />
-            <MobileNavButton href="/industries" text="Industries" onClick={() => toggleGradient('/industries')} />
-            <MobileNavButton href="/partnerships" text="Partnerships" onClick={() => toggleGradient('/partnerships')} />
-            <MobileNavButton href="/product-brochures" text="Product Brochures" onClick={() => toggleGradient('/product-brochures')} />
-            <MobileNavButton href="/case-studies" text="Case Studies" onClick={() => toggleGradient('/case-studies')} />
-            <MobileNavButton href="/videos" text="Videos" onClick={() => toggleGradient('/videos')} />
-            <MobileNavButton href="/blogs" text="Blogs" onClick={() => toggleGradient('/blogs')} />
-            <MobileNavButton href="/news" text="News" onClick={() => toggleGradient('/news')} />
-            <MobileNavButton href="/events" text="Events" onClick={() => toggleGradient('/events')} />
-          </div>
-        )} */}
         {isMenuOpen && (
           <div className='absolute ml-[268px] top-[20px]'>
               <DropdownMenu
